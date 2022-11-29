24a54d0ff4a3cce1f77640b5b9f19ecff675e5a0
hbmshell <hbmshell@gmail.com>
2022-11-28 22:44:45 -0300

    samples

--- samples/hello16InsideMP3.bat
+++ samples/hello16InsideMP3.bat
@@ -1,4 +1,4 @@
 @echo off
 echo Load inside mp3
 
-hbmshell https://raw.githubusercontent.com/hbmshell/core/main/aux_folder/1nn3rT4gs.mp3 --overwrite --tag-source #tag# --pause-after
+hbmshell --filename https://raw.githubusercontent.com/hbmshell/core/main/aux_folder/1nn3rT4gs.mp3 --overwrite --tag-source #tag# --pause-after
