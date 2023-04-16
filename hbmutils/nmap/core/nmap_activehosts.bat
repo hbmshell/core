603dc5f23aade4c96907c251d5777edb7f1d565b
hbmshell <hbmshell@gmail.com>
2023-04-15 23:10:58 -0300

    samples

--- hbmutils/nmap/core/nmap_activehosts.bat
+++ hbmutils/nmap/core/nmap_activehosts.bat
@@ -1,7 +1,7 @@
 @echo off
 if "%1" == "" (
-	hbmdialog nmap_activehosts.prg --virtual-include
+	hbmdialog nmap_activehosts.prg --virtual-include --pause-after
 ) else (
-	hbmdialog nmap_activehosts.prg --virtual-include -ip %1
+	hbmdialog nmap_activehosts.prg --virtual-include -ip %1 --pause-after
 )
  
